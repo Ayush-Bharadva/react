e0a4ea903046eebf08855cc374de0cde105f2933
Ayush-Bharadva <ayushkumar.bharadva@zuru.com>
2023-10-17 19:51:13 +0530

    modified

--- 15HigherOrderComponents/src/components/Internationalization/Login.jsx
+++ 15HigherOrderComponents/src/components/Internationalization/Login.jsx
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login({ translate, language, changeLanguage }) {
+	const [user, setUser] = useState("");
+	const [age, setAge] = useState("");
+	const [allUsers, setAllUsers] = useState([]);
+
+	const handleDuplicated = () => {
+		allUsers;
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (user !== null && (age > 0 || age < 120)) {
+			console.log(user);
+			console.log(age);
+			setAllUsers((prevUsers) => [
+				...prevUsers,
+				{ username: user, age: age },
+			]);
+			console.log("allUsers :", allUsers);
+		}
+	};
+
 	return (
 		<div>
 			<h2>{translate("Please Login")}</h2>
 			<p>Current language : {language}</p>
+
+			<form action="" onSubmit={handleSubmit}>
+				<input
+					style={{ fontSize: "1rem", margin: "0.5rem" }}
+					type="text"
+					placeholder="username"
+					value={user}
+					onChange={(e) => setUser(e.target.value)}
+				/>
+				<br />
+				<input
+					style={{ fontSize: "1rem", margin: "0.5rem" }}
+					type="number"
+					placeholder="age"
+					value={age}
+					onChange={(e) => setAge(e.target.value)}
+				/>
+				<br />
+				<button type="submit">Submit</button>
+			</form>
+
 			<p>Change language Below..</p>
 			<button onClick={() => changeLanguage("francias")}>francias</button>
 			<button onClick={() => changeLanguage("gujarati")}>gujarati</button>
